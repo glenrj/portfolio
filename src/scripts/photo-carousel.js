ed1b5e44ca30000ffff943e0b2ca0e58670f4671
Glen McCann <glenmccann2@gmail.com>
2024-02-13 15:07:15 -0500

    update tentatively complete, needs code cleanup

--- src/scripts/photo-carousel.js
+++ src/scripts/photo-carousel.js
@@ -14,15 +14,10 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    
-    if (n > slides.length - 1) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+      slides[i].style.display = "none";  
     }
-    slides[slideIndex].style.display = "flex";
-} 
+    slides[slideIndex-1].style.display = "flex";  
+  }
